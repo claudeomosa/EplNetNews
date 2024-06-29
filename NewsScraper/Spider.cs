803deaa60d48b933d96b607afa69eb388c5f1363
claudeomosa <claudeomosa@gmail.com>
2024-06-29 13:11:21 +0300

    Feat: Complete the spider implementation, returns top 14 news snippets

--- NewsScraper/Spider.cs
+++ NewsScraper/Spider.cs
@@ -1,8 +1,38 @@
 namespace NewsScraper;
+using HtmlAgilityPack;
+
 public static class Spider
 {
     public static void Init()
     {
         Console.WriteLine("Spider started Scraping");
+        string url = "https://www.skysports.com/premier-league-news";
+        List<string> newsSnippets = GetNewsSnippets(url);
+
+        if (newsSnippets.Count > 0)
+        {
+            foreach (string snippet in newsSnippets)
+            {
+                Console.WriteLine(snippet);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No news items found.");
+        }
     }
-} 
+
+    private static List<string> GetNewsSnippets(string url)
+    {
+        var web = new HtmlWeb();
+        var document = web.Load(url);
+        var newsItems = document.DocumentNode
+            .SelectNodes("//p")
+            .Where(node => node.Attributes["class"]?.Value.Trim() == "news-list__snippet")
+            .Take(14)
+            .Select(node => node.InnerText.Trim())
+            .ToList();
+
+        return newsItems;
+    }
+}
